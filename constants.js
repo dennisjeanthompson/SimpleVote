8414464498b49fd668728c2e146d70e57cdd834d
Kobita <161524322+kobita1122@users.noreply.github.com>
2026-01-24 15:18:54 +0600

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,5 @@
+// Replace this with your deployed contract address after running deploy.js
+module.exports = {
+    CONTRACT_ADDRESS: "0x0000000000000000000000000000000000000000",
+    GAS_LIMIT: 500000
+};
